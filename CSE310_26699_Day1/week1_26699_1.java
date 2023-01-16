9994a86a73d0e74f37645bfde23c9e6a9e31cc37
SSR100700 <72198465+SSR100700@users.noreply.github.com>
2023-01-16 21:07:14 +0530

    Create week1_26699_1.java

---
+++ CSE310_26699_Day1/week1_26699_1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x= sc.nextInt();
+        if(x<0){
+            System.out.print(-x);
+        }
+        else{
+            System.out.print(x);
+        }
+    }
+}
