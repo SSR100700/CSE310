4dffc8ec96d6a755ba74c82a1c53b56c4c03d8f5
Suraj Singh <72198465+SSR100700@users.noreply.github.com>
2023-01-23 22:26:08 +0530

    Create cse310_w2_2.java

---
+++ CSE310_26699_Day7/cse310_w2_2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int a = s.nextInt();
+        if(a>18) System.out.print("Eligible");
+        else System.out.print("Not eligible"); 
+    }
+}
