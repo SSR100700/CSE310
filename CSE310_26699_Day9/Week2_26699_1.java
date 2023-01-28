225f6f2c5d251e0d5b8950e8fdbb053f0774d4b0
Suraj Singh <72198465+SSR100700@users.noreply.github.com>
2023-01-28 09:58:00 +0530

    Create Week2_26699_1.java

---
+++ CSE310_26699_Day9/Week2_26699_1.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        if(a<=5){
+            System.out.print("thanks");
+        }
+        else{
+            System.out.print("101");
+        }
+    }
+}
