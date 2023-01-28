d5364b0379e88059fbd0347f00a1b87048d06a59
Suraj Singh <72198465+SSR100700@users.noreply.github.com>
2023-01-28 09:54:33 +0530

    Create Week2_26699_2.java

---
+++ CSE310_26699_Day9/Week2_26699_2.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.lang.Math;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a= sc.nextFloat();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        System.out.print((int)Math.ceil(a)+" "+b+" "+c);
+    }
+}
