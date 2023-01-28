587122e672548b4c4d0e8f55f440ad0c732ad966
Suraj Singh <72198465+SSR100700@users.noreply.github.com>
2023-01-28 19:39:58 +0530

    Create Cse310_20260_Week2_2.java

---
+++ CSE310_26699_Day10/Cse310_20260_Week2_2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String pno = sc.next();
+        int l = pno.length();
+        if(l==12){
+            System.out.print("Number Saved");
+        }
+        else System.out.print("Enter Valid Number");
+        
+    }
+}
