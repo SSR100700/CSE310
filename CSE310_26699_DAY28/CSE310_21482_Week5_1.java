a9ba618f4d0b67466cdce1f20ecde0eac4d4741d
Suraj Singh <72198465+SSR100700@users.noreply.github.com>
2023-02-22 22:46:25 +0530

    Create  CSE310_21482_Week5_1.java

---
+++ CSE310_26699_DAY28/CSE310_21482_Week5_1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String args[] ) throws Exception {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[10];
+        for(int i=0;i<10;i++){
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        for(int i=0;i<10;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
